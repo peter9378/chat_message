681d4e8123df425f561a96b35de9f374d628cec0
peter9378 <peter9378@naver.com>
2018-04-13 20:51:02 +0900

    add class [Message]

--- main/java/Message.java
+++ main/java/Message.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.sql.Timestamp;
 
 public class Message {
+    String text = "";
+    Timestamp timestamp;
+
+    public Message(){
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }
