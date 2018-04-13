e460f3613fcb962a7eb156c0f276935e3b34210e
peter9378 <peter9378@naver.com>
2018-04-13 20:49:43 +0900

    add class [User]

--- main/java/User.java
+++ main/java/User.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
-
 public class User {
+    String id = "";
+    String name = "";
+    String password = "";
+    int index = -1;
+
+    public User(){
+
+    }
+
+    public User(String id, String name, String password, int index){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.index = index;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public int getIndex(){
+        return this.index;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setIndex(int index){
+        this.index = index;
+    }
 }
