086e220a6db1a16962273b2ad7dcf28fb4ae8c95
peter9378 <peter9378@naver.com>
2018-04-13 20:52:39 +0900

    add class [UserDAO]

--- main/java/UserDAO.java
+++ main/java/UserDAO.java
@@ -1,2 +1,104 @@
-public class ClientDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    public UserDAO() {
+
+    }
+
+    private Connection getConnection() throws SQLException {
+        DBUtil dbUtil = new DBUtil();
+        Connection con;
+        con = dbUtil.getConnection();
+        return con;
+    }
+
+    public void addUser(User user) {
+        try {
+            String query = "INSERT into User(id, name, password, index) VALUES(?, ?, ?, ?)";
+
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user.getId());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getPassword());
+            preparedStatement.setInt(4, user.getIndex());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public void deleteUser(String id) {
+        try {
+            String queryString = "DELETE FROM User WHERE id=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public void getUserList() {
+        try {
+            String queryString = "SELECT * FROM User";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("id : " + resultSet.getString("id") +
+                "\nname : " + resultSet.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null)
+                    resultSet.close();
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
