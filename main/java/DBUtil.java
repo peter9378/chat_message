1e9945e6c2fd973589c872a2014f3953af8307f4
peter9378 <peter9378@naver.com>
2018-04-13 17:44:03 +0900

    add class [DBUtil] for using database(mysql)

--- main/java/DBUtil.java
+++ main/java/DBUtil.java
@@ -1,25 +1,50 @@
 import java.sql.*;
-import java.util.Scanner;
 
 public class DBUtil {
-    Connection con = null;
-    Statement statement = null;
-    ResultSet resultSet = null;
-    int result = 0;
+    private String url = "jdbc:mysql://210.89.188.165:3306";
+    private String user = "peter9378";
+    private String password = "peter";
+    private String driver = "com.mysql.jdbc.Driver";
 
-    try{
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e1) {
-            e1.printStackTrace();
-        }
-        try {
-            con = DriverManager.getConnection("jdbc:mysql://http://210.89.188.165:3306" +
-            "characterEncoding=utf-8", "doolda", "ehdwls");
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-        }
+    static Connection con = null;
+    static Statement statement = null;
+    static ResultSet resultSet = null;
 
-        Scanner scanner = new Scanner(System.in);
+
+    public DBUtil(){
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+    public DBUtil(String url, String user, String password, String driver) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.driver = driver;
+        new DBUtil();
+    }
+
+    public Connection getConnection()throws SQLException{
+
+        Connection con= null;
+        con = DriverManager.getConnection(url, user, password);
+
+        return con;
+    }
+
+    public void insert(String query){
+        try{
+            statement.executeUpdate(query);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(){
+
+    }
+
 }
