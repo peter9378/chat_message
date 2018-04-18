8a3a5821c304f900c291efcaae8139b248545056
peter9378 <peter9378@naver.com>
2018-04-18 21:32:41 +0900

    make package, add query function

--- main/java/user/UserDAO.java
+++ main/java/user/UserDAO.java
@@ -1,9 +1,13 @@
+package user;
+
 import myDB.DBUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UserDAO {
     Connection connection = null;
@@ -70,20 +74,20 @@ public class UserDAO {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         }
-
     }
 
     public LinkedList<User> getUserList() {
+        LinkedList<User> userList = new LinkedList<User>();
         try {
             String queryString = "SELECT * FROM User";
             connection = getConnection();
             preparedStatement = connection.prepareStatement(queryString);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                System.out.println("id : " + resultSet.getString("id") +
-                "\nname : " + resultSet.getString("name"));
+                userList.add(new User(resultSet.getString("id"), resultSet.getString("name"), resultSet.getInt("index")));
+                // print line
+                System.out.println("id : " + resultSet.getString("id") + "\nname : " + resultSet.getString("name"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -100,7 +104,62 @@ public class UserDAO {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         }
+        return userList;
+    }
+
+    public boolean isIdExist(String id){
+        try {
+            String queryString = "SELECT User.id FROM User WHERE id=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.wasNull()){
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+    public boolean isCorrectUser(String id, String password){
+        try{
+            String queryString = "SELECT User.password FROM User WHERE id=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            if(resultSet.getString("password").equals(password)){
+                return true;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 }
