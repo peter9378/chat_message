3898df981c72ded9f26becd890359286a47af742
peter9378 <peter9378@naver.com>
2018-04-23 12:59:30 +0900

    func idIdExist was not working correctly. Now fixed.

--- main/java/user/UserDAO.java
+++ main/java/user/UserDAO.java
@@ -2,6 +2,7 @@ package user;
 
 import myDB.DBUtil;
 
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,7 +34,7 @@ public class UserDAO {
             preparedStatement.setString(1, user.getId());
             preparedStatement.setString(2, user.getName());
             preparedStatement.setString(3, user.getPassword());
-            preparedStatement.setString(4, user.getStatus().toString());
+            preparedStatement.setString(4, user.getStatus());
             preparedStatement.setInt(5, user.getIndex());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -85,9 +86,8 @@ public class UserDAO {
             preparedStatement = connection.prepareStatement(queryString);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                userList.add(new User(resultSet.getString("id"), resultSet.getString("name"), resultSet.getInt("index")));
-                // print line
-                System.out.println("id : " + resultSet.getString("id") + "\nname : " + resultSet.getString("name"));
+                userList.add(new User(resultSet.getString("id"), resultSet.getString("name"),
+                        "", resultSet.getString("status"), resultSet.getInt("index")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -110,13 +110,14 @@ public class UserDAO {
 
     public boolean isIdExist(String id){
         try {
-            String queryString = "SELECT User.id FROM User WHERE id=?";
+            String queryString = "SELECT * FROM User WHERE id=?";
             connection = getConnection();
             preparedStatement = connection.prepareStatement(queryString);
             preparedStatement.setString(1, id);
             resultSet = preparedStatement.executeQuery();
-            if(resultSet.wasNull()){
-                return false;
+            resultSet.next();
+            if(resultSet.getString("id").equals(id)){
+                return true;
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -132,7 +133,7 @@ public class UserDAO {
                 e.printStackTrace();
             }
         }
-        return true;
+        return false;
     }
 
     public boolean isCorrectUser(String id, String password){
@@ -188,5 +189,202 @@ public class UserDAO {
             }
         }
         return name;
+    }
+
+    public int getIndexById(String id){
+        int index = -1;
+        try{
+            String queryString = "SELECT * FROM User WHERE id=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            index = resultSet.getInt("index");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return index;
+    }
+
+    public int getIndexByName(String name){
+        int index = -1;
+        try{
+            String queryString = "SELECT * FROM User WHERE name=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, name);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            index = resultSet.getInt("index");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return index;
+    }
+
+    public boolean isOnlineUserByName(String name){
+        try{
+            String queryString = "SELECT * FROM User WHERE name=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, name);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            if(resultSet.getString("status").equals("online")){
+                return true;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public void updateUserStatus(String id, String status){
+        try{
+            String queryString = "UPDATE User set status=? WHERE id=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, status);
+            preparedStatement.setString(2, id);
+            preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setUserIndexById(int index, String id){
+        try{
+            String queryString = "UPDATE User set User.index=? WHERE id=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setInt(1, index);
+            preparedStatement.setString(2, id);
+            preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setUserIndexByName(int index, String name){
+        try{
+            String queryString = "UPDATE User set User.index=? WHERE name=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setInt(1, index);
+            preparedStatement.setString(2, name);
+            preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getStatusByName(String name){
+        String status = "";
+        try{
+            String queryString = "SELECT * FROM User WHERE name=?";
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(queryString);
+            preparedStatement.setString(1, name);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            status = resultSet.getString("status");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return status;
+    }
+
+
+    public String getSHA256(String string) throws Exception {
+        StringBuffer sb = new StringBuffer();
+
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(string.getBytes());
+
+        byte[] messageString = messageDigest.digest();
+        for (int i = 0; i < messageString.length; i++) {
+            byte tempByte = messageString[i];
+            String tmp = Integer.toString((tempByte & 0xff) + 0x100, 16).substring(1);
+            sb.append(tmp);
+        }
+        return sb.toString();
     }
 }
